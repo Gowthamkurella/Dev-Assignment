ac583563e5c3942cd8b3205b0d9047662dc8733b
Sri Charan <sricharan@campx.in>
2024-05-15 19:19:39 +0530

    changes

--- backend/src/rating/dto/rate-questions.dto.ts
+++ backend/src/rating/dto/rate-questions.dto.ts
@@ -1,10 +1,10 @@
 import { Type } from 'class-transformer';
-import { ValidateNested, IsArray } from 'class-validator';
+import { IsArray, ValidateNested } from 'class-validator';
 import { RateQuestionDto } from './rate-question.dto';
 
 export class RateQuestionsDto {
-    @IsArray()
-    @ValidateNested({ each: true })
-    @Type(() => RateQuestionDto)
-    rates: RateQuestionDto[];
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => RateQuestionDto)
+  rates: RateQuestionDto[];
 }
