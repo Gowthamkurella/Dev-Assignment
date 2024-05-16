ac8c285a10b885d5cbaf3992b0901e9533a157e8
K.N.V.GOWTHAM <110026459+Gowthamkurella@users.noreply.github.com>
2024-05-16 14:53:47 +0530

    Delete backend/dist directory

--- backend/dist/rating/dto/rate-question.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class RateQuestionDto {
-    questionId: string;
-    rating: number;
-}
--- backend/dist/rating/dto/rate-question.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RateQuestionDto = void 0;
-class RateQuestionDto {
-}
-exports.RateQuestionDto = RateQuestionDto;
-//# sourceMappingURL=rate-question.dto.js.map
