57744b417a28deb3858c963a92cc51b2f8b96af5
Sri Charan <sricharan@campx.in>
2024-05-17 10:33:53 +0530

    changes

--- backend/src/rating/rating.controller.ts
+++ backend/src/rating/rating.controller.ts
@@ -1,31 +1,46 @@
-import { Controller, Post, Body, Param, Get, Query,Res, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { Response } from 'express';
-import { RatingService } from './rating.service';
 import { RateQuestionsDto } from './dto/rate-questions.dto';
+import { RatingService } from './rating.service';
 
 @Controller('ratings')
 export class RatingController {
-    constructor(private readonly ratingService: RatingService) {}
+  constructor(private readonly ratingService: RatingService) {}
 
-    @Post('feedback')
-    addRatings(@Body() rateQuestionsDto: RateQuestionsDto) {
-        return this.ratingService.addRatings(rateQuestionsDto.rates);
+  @Post('feedback')
+  addRatings(@Body() rateQuestionsDto: RateQuestionsDto) {
+    return this.ratingService.addRatings(rateQuestionsDto.rates);
+  }
+
+  @Get()
+  async getQuestionsWithRatings(@Res() res: Response) {
+    const questions = await this.ratingService.getQuestionsWithRatings();
+    if (questions.length === 0) {
+      return res.status(HttpStatus.NO_CONTENT).send();
     }
+    return res.status(HttpStatus.OK).json(questions);
+  }
 
-    @Get()
-    async getQuestionsWithRatings(@Res() res: Response) {
-      const questions = await this.ratingService.getQuestionsWithRatings();
-      if (questions.length === 0) {
-        return res.status(HttpStatus.NO_CONTENT).send();
-      }
-      return res.status(HttpStatus.OK).json(questions);
-    }
-
-    @Get('date-range')
-    async getQuestionsWithRatingsByDate(@Query('start') start: string, @Query('end') end: string, @Res() res: Response) {
+  @Get('date-range')
+  async getQuestionsWithRatingsByDate(
+    @Query('start') start: string,
+    @Query('end') end: string,
+    @Res() res: Response,
+  ) {
     const startDate = new Date(start);
     const endDate = new Date(end);
-    const questions = await this.ratingService.getQuestionsWithRatingsByDate(startDate, endDate);
+    const questions = await this.ratingService.getQuestionsWithRatingsByDate(
+      startDate,
+      endDate,
+    );
     if (questions.length === 0) {
       return res.status(HttpStatus.NO_CONTENT).send();
     }
